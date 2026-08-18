48049353faaf5cd006f11cb61807abeaef9ffb5c
Praful Bowade <prafulbowade73@gmail.com>
2026-08-18 17:45:04 +0530

    Add PIN verification to ATM machine

--- atm-machine-project.py
+++ atm-machine-project.py
@@ -1,38 +1,63 @@
 balance = 10000
+correct_pin = 1234
+attempts = 3
 
-while True:
-    print("=====ATM MACHINE=====")
-    print("check balance")
-    print("deposit money")
-    print("withdraw money")
-    print("exit")   
+# PIN verification
+while attempts > 0:
+    pin = int(input("Enter your PIN: "))
 
-    choice = int(input("enter your choice: "))
+    if pin == correct_pin:
+        print("Login successful!")
+        break
+    else:
+        attempts -= 1
+        print("Wrong PIN!")
+        print("Attempts remaining:", attempts)
 
+if attempts == 0:
+    print("Your card is blocked.")
+else:
 
-    if choice == 1:
-        print("your current balanceis:",balance)
+    # ATM Menu
+    while True:
+        print("\n===== ATM MACHINE =====")
+        print("1. Check Balance")
+        print("2. Deposit Money")
+        print("3. Withdraw Money")
+        print("4. Exit")
 
-    elif choice == 2:
-        amount  = int(input("enter deposit amount:"))
-        balance += amount
-        print("amount seposited successfully:")
-        print("update balance is:",balance)
+        choice = int(input("Enter your choice: "))
 
-    elif choice == 3:
-        amount = int(input("enter withdraw amount:"))
-        if amount <= balance:
-           balance -= amount
-           print("please collect your cash.")
-           print("remaining balance is:",balance)
+        if choice == 1:
+            print("Current balance:", balance)
 
+        elif choice == 2:
+            amount = int(input("Enter deposit amount: "))
+
+            if amount > 0:
+                balance += amount
+                print("Amount deposited successfully.")
+                print("Updated balance:", balance)
+            else:
+                print("Enter a valid amount.")
+
+        elif choice == 3:
+            amount = int(input("Enter withdrawal amount: "))
+
+            if amount <= 0:
+                print("Enter a valid amount.")
+
+            elif amount <= balance:
+                balance -= amount
+                print("Please collect your cash.")
+                print("Remaining balance:", balance)
+
+            else:
+                print("Insufficient balance.")
+
+        elif choice == 4:
+            print("Thank you for using ATM.")
+            break
 
         else:
-            print("insufficent balance")
-
-    elif choice == 4:
-        print("thank you for using ATM.")
-        break
-
-    else:
-        print("invalid choice please try again.")
+            print("Invalid choice. Please try again.")
